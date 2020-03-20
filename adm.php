534f03bba8d321c6f44062cac8cfdefb451a29f3
Avignon Camille <camille.avignon15@gmail.com>
2020-03-20 14:37:27 +0100

    Page admin

---
+++ adm.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <!-- Ici se trouvent les métadonnées pour le navigateur -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        
+
+        <!-- Ici se trouvent les métadonnées pour le moteur de recherche (et le navigateur) -->
+        <title>CDA Hotel Nice</title>
+        <meta
+            name="description"
+            content="Ce petit paragraphe sera affiché dans les SERP, juste sous le titre des pages."
+        />
+
+        <!-- Ici se trouvent les liens vers les fichiers favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png">
+        <link rel="manifest" href="site.webmanifest">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+
+        <!-- Ici se trouvent les liens vers les feuilles de styles -->
+        <link rel="stylesheet" href="assets/css/styles.min.css" />
+    </head>
+
+    <body>
+
+        <header>
+            <a href="index.html"><img src="assets/img/logonice_mini2.png" height="90px" alt="CDANICE"/></a>
+            <p>Administration<p>
+            <p class="menu">Réservations</p>
+            <p class="menu">Chambres</p>
+            <p class="menu">Liste Client</p>
+            <p class="menu">Mon compte</p>
+        </header>
+
+        <main>
+            <section>
+
+                <p>Listes</p>
+                <p>Calendrier</p>
+                <p>Statistiques</p>
+                
+            </section>
+
+        </main>
+
+    </body>
+</html>
