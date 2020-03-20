a368e8b21fb45de6a138c240482394624f5dd6be
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2020-03-20 15:37:13 +0100

    ajout de la page mon compte + css

---
+++ managment/my_account.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <!-- Ici se trouvent les métadonnées pour le navigateur -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+
+
+    <!-- Ici se trouvent les métadonnées pour le moteur de recherche (et le navigateur) -->
+    <title>CDA Hotel Nice - Administration - Mon compte</title>
+    <meta
+        name="description"
+        content="Ce petit paragraphe sera affiché dans les SERP, juste sous le titre des pages."
+    />
+
+    <!-- Ici se trouvent les liens vers les fichiers favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="../apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="../favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="../favicon-16x16.png">
+    <link rel="manifest" href="../site.webmanifest">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
+    <!-- Ici se trouvent les liens vers les feuilles de styles -->
+    <link rel="stylesheet" href="../assets/css/managment.min.css" />
+</head>
+<body>
+    <header>
+        <a href="index.html"><img src="../assets/img/logonice_mini2.png" height="90px" alt="CDANICE"/></a>
+        <h1><a href="index.html">Administration</a></h1>
+    </header>
+    <main>
+        <nav class="side-nav">
+            <a href="">test</a>
+            <a href="">test</a>
+            <a href="">test</a>
+        </nav>
+    </main>
+</body>
