9b36547e839656e4222a728f812c43bec0489f8f
combo baraka <combo.baraka01@gmail.com>
2020-05-19 15:53:42 +0200

    creation du dossier controller avec les fichiers chambre et reservation

---
+++ controller/chambreController.php
@@ -0,0 +1,13 @@
+<?php
+
+function listeAction(){
+    echo '
+        <h1>Liste des chambres</h1>
+    ';
+}
+
+function detailAction(){
+    echo '
+        <h1>Détails de la chambre</h1>
+    ';
+}
