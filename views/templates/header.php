4cf18f9b3f1a8bb011ca601dba4440e3bdac3b70
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2020-05-26 11:33:20 +0200

    correction webmanifest et favicon pour chrome

--- views/templates/header.php
+++ views/templates/header.php
@@ -15,10 +15,10 @@
         />
 
         <!-- Ici se trouvent les liens vers les fichiers favicons -->
-        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png"/>
-        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"/>
-        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"/>
-        <link rel="manifest" href="site.webmanifest"/>
+        <link rel="apple-touch-icon" sizes="180x180" href="<?php echo SITE_DIR;?>apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="<?php echo SITE_DIR;?>favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="<?php echo SITE_DIR;?>favicon-16x16.png"/>
+        <link rel="manifest" href="<?php echo SITE_DIR;?>site.webmanifest"/>
         <meta name="msapplication-TileColor" content="#da532c"/>
         <meta name="theme-color" content="#ffffff"/>
 
