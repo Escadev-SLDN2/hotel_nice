396f0a39f47d5ce56fb9cffd1cd86fe6e9d882f7
DesirThomas <thomasdesir.pro@gmail.com>
2020-05-26 17:48:26 +0200

    mvc

--- controllers/ChambreController.php
+++ controllers/ChambreController.php
@@ -1,6 +1,10 @@
 <?php
+require('models/Chambre.php');
 
 function indexAction() {
+    $chambre = new Chambre();
+    $chambres = $chambre->getChambres();
+
     $pageTitle = 'Liste des chambres';
     require('views/chambre/index.php');
 }
--- views/chambre/index.php
+++ views/chambre/index.php
@@ -1,3 +1,8 @@
 <?php require('views/templates/header.php');?>
-    <h1>Liste des chambres</h1>
+<h1>Liste des chambres</h1>
+<ul>
+    <?php foreach ($chambres as $chambre): ?>
+        <li><?php echo $chambre['type']; ?></li>
+    <?php endforeach; ?>
+</ul>
 <?php require('views/templates/footer.php');?>
