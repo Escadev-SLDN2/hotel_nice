2510445f503ab6f4f51370482c47432e33a450dd
DesirThomas <thomasdesir.pro@gmail.com>
2020-05-28 17:41:49 +0200

    Avancement

--- views/chambre/index.php
+++ views/chambre/index.php
@@ -6,18 +6,42 @@
     <?php foreach ($chambres as $chambre) : ?>
         <section class="row reverse">
             <div class="block-content large-block color-sky">
+
+                <!--Type de Chambre-->
                 <h2><?php echo ucfirst($chambre['type']); ?></h2>
+
+                <!--Description-->
                 <div class="description">
                     <?php echo $chambre['description']; ?>
                 </div>
+
+                <!--Service-->
                 <div class="tarif">
-                    <?php echo $chambre['tarif1']; ?> / <?php echo $chambre['tarif2']; ?> / <?php echo $chambre['tarif3']; ?>
+                    <ul><span class="bold">Services :</span>
+                    <?php
+                    $services = explode(';', $chambre['services']); 
+                    foreach($services as $service) : ?>
+                        <li><?php echo ucfirst($service) ?></li>
+                    <?php endforeach; ?>
+                    </ul>
                 </div>
+
+                <!--Saisons et Tarif-->
+                <div class="tarif">
+                    <p><span class="bold">Basse saison</span> <?php echo number_format($chambre['tarif1'], 0, ',', ' '); ?> &euro;
+                    <span class="bold">/ Moyenne saison :</span> <?php echo number_format($chambre['tarif2'], 0, ',', ' '); ?> &euro;
+                    <span class="bold">/ Haute saison :</span> <?php echo number_format($chambre['tarif3'], 0, ',', ' '); ?> &euro;</p>
+                </div>
+
+                <!--Image-->
                 <div class="etat-reserv">
                     <?php echo $chambre['etat']; ?>
-                    <button type="button" id="btnReserv">Administration</button>
+                    <button type="button" id="btnReserv">Réserver</button>
                 </div>
+
             </div>
+
+            <!--Image-->
 			<div class="block-content medium-block">
                 <img src="<?php echo SITE_DIR; ?>assets/img/<?php echo $chambre['img']; ?>" alt="<?php echo $chambre['type']; ?>" />
             </div>
