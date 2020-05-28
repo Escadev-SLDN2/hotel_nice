ad45b6240f7f7ad50d26b8e43bd58c2f36b0a41e
combo baraka <combo.baraka01@gmail.com>
2020-05-28 14:29:34 +0200

    autoformate

--- views/chambre/index.php
+++ views/chambre/index.php
@@ -1,9 +1,9 @@
-<?php require('views/templates/header.php');?>
+<?php require('views/templates/header.php'); ?>
 
 <div class="container">
     <h1>Liste des chambres</h1>
 
-    <?php foreach ($chambres as $chambre): ?>
+    <?php foreach ($chambres as $chambre) : ?>
         <section class="chambre">
             <div class="photo">
                 <img src="<?php echo SITE_DIR; ?>assets/img/<?php echo $chambre['img']; ?>" alt="<?php echo $chambre['type']; ?>" />
@@ -13,13 +13,10 @@
                 <div class="description">
                     <?php echo $chambre['description']; ?>
                 </div>
-                <div class="services">
-                    
-                </div>
             </div>
         </section>
     <?php endforeach; ?>
 
 </div>
 
-<?php require('views/templates/footer.php');?>
+<?php require('views/templates/footer.php'); ?>
