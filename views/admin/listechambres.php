2be0ba6042a78eca215f43131bfa37a2706712e9
DesirThomas <thomasdesir.pro@gmail.com>
2020-05-29 11:45:18 +0200

    correction nav et mise en page

--- views/admin/listechambres.php
+++ views/admin/listechambres.php
@@ -4,35 +4,35 @@
     <h1>Liste des chambres</h1>
 	<section class="row">
 		<div class="block-content large-block color-sky centered">
-			<table width="100%">
-			<thead>
-				<tr>
-					<th>id</th>
-					<th>état</th>
-					<th>type</th>
-					<th>tarif1</th>
-					<th>tarif2</th>
-					<th>tarif3</th>
-					<th>&nbsp;</th>
-				</tr>
-			</thead>
+			<table>
+				<thead>
+					<tr>
+						<th>id</th>
+						<th>état</th>
+						<th>type</th>
+						<th>tarif1</th>
+						<th>tarif2</th>
+						<th>tarif3</th>
+						<th>&nbsp;</th>
+					</tr>
+				</thead>
 
-			<tbody>
-				<?php foreach($chambres as $chambre): ?>
-				<tr>
-					<td><?php echo $chambre['id']; ?></td>
-					<td><?php echo $chambre['etat']; ?></td>
-					<td><?php echo $chambre['type']; ?></td>
-					<td><?php echo $chambre['tarif1']; ?></td>
-					<td><?php echo $chambre['tarif2']; ?></td>
-					<td><?php echo $chambre['tarif3']; ?></td>
-					<td><a href="<?php echo SITE_DIR; ?>admin/editchambre/<?php echo $chambre['id']; ?>"><i class="fas fa-edit"></i></a></td>
-				</tr>
-				<?php endforeach; ?>
-			</tbody>
+				<tbody>
+					<?php foreach($chambres as $chambre): ?>
+					<tr>
+						<td><?php echo $chambre['id']; ?></td>
+						<td><?php echo $chambre['etat']; ?></td>
+						<td><?php echo $chambre['type']; ?></td>
+						<td><?php echo $chambre['tarif1']; ?></td>
+						<td><?php echo $chambre['tarif2']; ?></td>
+						<td><?php echo $chambre['tarif3']; ?></td>
+						<td><a href="<?php echo SITE_DIR; ?>admin/editchambre/<?php echo $chambre['id']; ?>"><i class="fas fa-edit"></i></a></td>
+					</tr>
+					<?php endforeach; ?>
+				</tbody>
 
-			<tfoot></tfoot>
-		</table>
+				<tfoot></tfoot>
+			</table>
 		</div>
 	</section>
     
