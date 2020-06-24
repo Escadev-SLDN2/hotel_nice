fc4fe5ebd4ca8f4038fae49030a5252b097d5bea
Désir Thomas <thomasdesir.pro@gmail.com>
2020-06-24 11:13:56 +0200

    Chambre.php, the return

---
+++ models/Chambre.php
@@ -0,0 +1,51 @@
+<?php
+require('models/Bdd.php');
+
+
+class Chambre
+{
+    public function getChambres()
+    {
+        $bdd = new Bdd();
+        $connection = $bdd->getConnection();
+        $result = $connection->query('SELECT * FROM chambre ORDER BY id ASC');
+        return $result ? $result->fetchAll(PDO::FETCH_ASSOC) : null;
+    }
+
+    public function getChambre($chambreId)
+    {
+        $bdd = new Bdd();
+        $connection = $bdd->getConnection();
+        $result = $connection->query('SELECT * FROM chambre where id = ' . $chambreId);
+        return $result ? $result->fetch(PDO::FETCH_ASSOC) : null;
+    }
+
+    public function changeChambre($chambreId, $params)
+    {
+        $bdd = new Bdd();
+        $connection = $bdd->getConnection();
+        $result = $connection->prepare("UPDATE chambre SET etat = :etat , chambre = :chambre , tarif1 = :tarif1 , tarif2 = :tarif2 , tarif3 = :tarif3 where id = $chambreId");
+        $result -> execute($params);
+    }
+
+    public function ajouterChambre( $params )
+    {
+        $bdd        = new Bdd();
+        $connection = $bdd->getConnection();
+
+        $result =
+            $connection->prepare( 'INSERT INTO chambre(etat, chambre, description, services, img, info, tarif1, tarif2, tarif3) VALUES(:etat, :chambre, :description, :services, :img, :info, :tarif1, :tarif2, :tarif3 )' );
+        $result->execute( $params );
+    }
+
+    public function supprimerChambre( $id )
+    {
+        $bdd        = new Bdd();
+        $connection = $bdd->getConnection();
+
+        $result = $connection->prepare( 'DELETE FROM chambre WHERE id = :id' );
+        $result->execute( array(
+            'id' => $id,
+        ) );
+    }
+}
